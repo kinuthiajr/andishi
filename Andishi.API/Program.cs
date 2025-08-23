0a254b7ee21e9659872b0d95cb839cc6c30665fb
smk <kinuthiajr@gmail.com>
2025-08-23 23:24:13 +0300

    EB deploy

--- Andishi.API/Program.cs
+++ Andishi.API/Program.cs
@@ -16,18 +16,18 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<AndishiDbContext>(opt => opt.UseNpgsql(builder.Configuration.
-    GetConnectionString("NEON_CONNECTION_STRING")
+    GetConnectionString("DefaultConnectionString")
 ));
 
 var app = builder.Build();
 
 // Apply database migrations automatically on startup.
 // This is a convenient way to ensure the database schema is up-to-date in Azure.
-using (var scope = app.Services.CreateScope())
-{
-    var dbContext = scope.ServiceProvider.GetRequiredService<AndishiDbContext>();
-    dbContext.Database.Migrate();
-}
+// using (var scope = app.Services.CreateScope())
+// {
+//     var dbContext = scope.ServiceProvider.GetRequiredService<AndishiDbContext>();
+//     dbContext.Database.Migrate();
+// }
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
